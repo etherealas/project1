e5e0eea90fabfa72e4b4547944cdd32375dd7db2
Alexandr <ethereal.as@yandex.ru>
2023-05-09 16:54:27 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,19 @@
+
+let incr = 10,
+    decr = 10;
+
+console.log(++incr);
+console.log(--decr);
+
+
+console.log(5%2);
+
+console.log(2+4*2 == 8);
+
+&&
+||
+
+const isChecked = true,
+      isClose = true;
+
+console.log(isChecked || !isClose);
